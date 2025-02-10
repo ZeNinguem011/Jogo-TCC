dcf5fb0d270a42366fa91ee1f3d32644000e1c1c
Gamexzy <115941273+Gamexzy@users.noreply.github.com>
2025-02-09 22:52:09 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
 
 const perguntas = [
   // QUESTÕES FÁCEIS (14 questões)
